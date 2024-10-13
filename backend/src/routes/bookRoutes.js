2396e72a735c32f23622b5652c33dbe0d5591dac
Ritik Trivedi <ritiktrivedi902@gmail.com>
2024-10-13 12:46:33 +0530

    backend updated

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const bookController = require('../controller/bookController');
-router.post('/createBook',bookController.createBook);
-router.get('/getAllBook',bookController.getAllBooks);
-router.get('/getBookById/:id',bookController.getBookById);
-router.put('/updateBook/:id',bookController.updateBook);
-router.delete('/deleteBook/:id',bookController.deleteBook);
+router.post('/',bookController.createBook);
+router.get('/',bookController.getAllBooks);
+router.get('/:id',bookController.getBookById);
+router.put('/:id',bookController.updateBook);
+router.delete('/:id',bookController.deleteBook);
 
 
 module.exports = router;
